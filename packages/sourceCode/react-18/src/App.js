38b064029460b0289452284ea76809c750a99915
youjiaqi <2430284055@qq.com>
2025-04-02 16:45:14 +0800

    fix: baogao

--- packages/sourceCode/react-18/src/App.js
+++ packages/sourceCode/react-18/src/App.js
@@ -1,20 +1,19 @@
-import logo from './logo.svg'
-import './App.css'
-
+import {useState, useEffect} from 'react'
 function App() {
+	const [count, setCount] = useState(-1)
+
+	useEffect(() => {
+		setCount(0)
+	}, [])
+
+	console.log('x') //sy-log
+
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
+		<div>
+			<h3>SetStatePage</h3>
 		</div>
 	)
 }
 
 export default App
+
